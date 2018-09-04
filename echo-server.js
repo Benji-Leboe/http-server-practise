021af099fa1a17f22890c9ce0a2d8ed7ae452bdc
Benji-Leboe <benjamin.r.leboe@gmail.com>
2018-09-04 18:54:34 +0000

    add error handling

--- echo-server.js
+++ echo-server.js
@@ -1,6 +1,14 @@
 const http = require('http');
 
 http.createServer((req, res) => {
+  req.on('error', (err) => {
+    console.error(err);
+    res.statusCode = 400;
+    res.end();
+  });
+  res.on('error', (err) => {
+    console.error(err);
+  });
   if(req.method === 'POST' && req.url === '/echo'){
     req.pipe(res);
   }else{
