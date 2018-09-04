28441d533104452aaa59973d658939e3dcbd1f1f
Benji-Leboe <benjamin.r.leboe@gmail.com>
2018-09-04 19:03:00 +0000

    add my-server

---
+++ my-server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const port = 8080;
+
+function requestHandler(req, res){
+  if(req.url === '/'){
+    res.end('Welcome to the homepage!');
+  }else if(req.url === '/urls'){
+    res.end('www.lighthouselabbs.ca\nwww.google.ca');
+  }else{
+    res.statusCode = 404;
+    res.end('Invalid path');
+  }
+}
+
+let server = http.createServer(requestHandler);
+
+server.listen(port, () => {
+  console.log(`Server listening on: http://localhost:${port}`);
+});
